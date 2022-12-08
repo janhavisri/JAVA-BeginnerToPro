677237bd1d6c54860fedf7d0e18cb103600a7d49
janhavisri <janhavis830@gmail.com>
2022-12-08 08:01:45 +0530

    commit

--- Arrays.java
+++ Arrays.java
@@ -190,20 +190,33 @@ public class Arrays{
 
 // **********************PRINT SUBARRAY****************
 public static void printSubarray(int numbers[]){
+    int ts = 0;
+    int currSum = 0;
+    int maxSum = Integer.MIN_VALUE;
+    
     for(int i=0; i<numbers.length; i++){
         int start = i;
-        for(int j=i+1; j<numbers.length; j++ ){
+        
+        for(int j=i; j<numbers.length; j++ ){
             int end = j;
+            currSum = 0;
             for(int k=start; k<=end; k++){
-                System.out.print(numbers[k]+" ");
+                // System.out.print(numbers[k] + " " );
+                //subarray sum
+                currSum += numbers[k];
             }
-            System.out.println();
+            ts++;
+            System.out.println(currSum);
+            if(maxSum < currSum){
+                maxSum = currSum;
+            }
         }
-        System.out.println();
     }
+    System.out.println("Max Sum:" + maxSum);
+    System.out.println("Total Subarray:" + ts);
 }
 public static void main(String args[]){
-       int numbers[] = {2, 4, 6, 8, 10};
+       int numbers[] = {1, -2, 6, -1, 3};
         printSubarray(numbers);
 }
 }
